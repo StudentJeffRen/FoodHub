3b42c1ee3474a5fba132881ee22e0d4e600e5fa1
StudentJeffRen <2205953570@qq.com>
2019-12-07 23:37:58 +0800

    实现收藏，cloud里修改local数据

--- FoodHub/CloudView.swift
+++ FoodHub/CloudView.swift
@@ -10,13 +10,14 @@ import SwiftUI
 
 struct RestaurantListCloud: View {
     @EnvironmentObject var cloudData: SharedData
+    @EnvironmentObject var localData: UserData
     
     var body: some View {
-        
         NavigationView {
             List(cloudData.sharedRestaurants) { restaurant in
                 RestaurantCloudRow(restaurant: restaurant)
                     .environmentObject(self.cloudData)
+                    .environmentObject(self.localData)
             }
             
             .navigationBarTitle(Text("Cloud"))
@@ -28,5 +29,6 @@ struct RestaurantListCloud_Previews: PreviewProvider {
     static var previews: some View {
             RestaurantListCloud()
                 .environmentObject(SharedData(from: restaurantCloudData))
+                .environmentObject(UserData(from: restaurantLocalData))
     }
 }
