92bbd3d2837cfec012f6f693977b0a21b2a5b229
StudentJeffRen <2205953570@qq.com>
2019-12-13 01:09:13 +0800

    去除老版本无用代码

--- FoodHub/Supporting Files/SessionStore.swift
+++ FoodHub/Supporting Files/SessionStore.swift
@@ -11,8 +11,7 @@ import Firebase
 import Combine
 
 class SessionStore: ObservableObject {
-    var didChange = PassthroughSubject<SessionStore, Never>()
-    @Published var session: User? { didSet { self.didChange.send(self) }}
+    @Published var session: User?
     var handle: AuthStateDidChangeListenerHandle?
     
     func listen() {
