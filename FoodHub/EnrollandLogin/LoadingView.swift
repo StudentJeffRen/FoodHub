1c3e23b7560dc05e07e8c1069dd7dbdbf801e4ab
StudentJeffRen <2205953570@qq.com>
2019-12-13 01:04:15 +0800

    加载界面

---
+++ FoodHub/EnrollandLogin/LoadingView.swift
@@ -0,0 +1,39 @@
+//
+//  LoadingView.swift
+//  FoodHub
+//
+//  Created by JeffRen on 2019/12/8.
+//  Copyright © 2019 JeffRen. All rights reserved.
+//
+
+import SwiftUI
+
+struct LoadingView<Content>: View where Content: View {
+
+    @Binding var isShowing: Bool
+    var content: () -> Content
+
+    var body: some View {
+        GeometryReader { geometry in
+            ZStack(alignment: .center) {
+
+                self.content()
+                    .disabled(self.isShowing)
+                    .blur(radius: self.isShowing ? 3 : 0)
+
+                VStack {
+                    Text("Loading...")
+                    ActivityIndicator(isAnimating: .constant(true), style: .large)
+                }
+                .frame(width: geometry.size.width / 2,
+                       height: geometry.size.height / 5)
+                .background(Color.secondary.colorInvert())
+                .foregroundColor(Color.primary)
+                .cornerRadius(20)
+                .opacity(self.isShowing ? 1 : 0)
+
+            }
+        }
+    }
+
+}
